9fc4d52286aa3ee1652240f6a1095322bceebf2d
Joshua Eze <ezejoshuac@gmail.com>
2023-01-17 21:26:41 +0100

    Add project files.

---
+++ CustomDocumentAttributes/DocumentAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomDocumentAttributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class DocumentAttributes : Attribute
+    {
+        public string Description { get; set; }
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public DocumentAttributes(string description, string input = null, string output = null)
+        {
+            Description = description;
+            Input = input;
+            Output = output;
+        }
+    }
+}
