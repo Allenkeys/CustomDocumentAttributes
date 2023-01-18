d4817ded0893d4dd0eaede4ca33f505504e43b93
Joshua Eze <ezejoshuac@gmail.com>
2023-01-18 08:57:33 +0100

    Added GetDocs method to display types of the assembly

---
+++ CustomDocumentAttributes/DocumentAttributeOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomDocumentAttributes
+{
+    internal class DocumentAttributeOperations
+    {
+        public static void GetDocs()
+        {
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var members = from assembly in assemblies
+                          from type in assembly.GetTypes()
+                          select type.GetMembers();
+
+            foreach (var memberArr in members)
+            {
+                foreach (var member in memberArr)
+                {
+                    var attributes = member.GetCustomAttributes(typeof(DocumentAttributes), true);
+
+                    if (attributes.Length > 0)
+                    {
+                        var doc = (DocumentAttributes)attributes[0];
+                        Console.WriteLine($"Member Type: {member.MemberType}");
+                        Console.WriteLine($"Name: {member.Name}");
+                        Console.WriteLine($"\nDescription: {doc.Description}\nInput: {doc.Input}\n Output: {doc.Output}");
+
+                    }
+                }
+            }
+        }
+    }
+}
